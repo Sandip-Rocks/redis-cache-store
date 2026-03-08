93c84dc7289d2144ea2e699820748bd88a0e979e
Sandip Das <sandip4991@gmail.com>
2026-03-08 14:14:58 +0530

    feature: Add docker for redis server and load env variables through config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CacheModule } from '@nestjs/cache-manager';
 import { HomepageModule } from './homepage/homepage.module';
+import { redisStore } from 'cache-manager-redis-store';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    CacheModule.register({
-      ttl: 300000, // 5 minutes
+    ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get<string>('REDIS_HOST', '127.0.0.1'),
+        port: configService.get<number>('REDIS_PORT', 6379),
+        ttl: 300,
+      }),
+      inject: [ConfigService],
     }),
     HomepageModule,
   ],
--- src/homepage/homepage.controller.ts
+++ src/homepage/homepage.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { HomepageService } from './homepage.service';
-import { CacheInterceptor } from '@nestjs/cache-manager';
+import { CacheInterceptor, CacheKey, CacheTTL } from '@nestjs/cache-manager';
 
 @UseInterceptors(CacheInterceptor)
 @Controller('homepage')
 export class HomepageController {
   constructor(private readonly homepageService: HomepageService) {}
 
+  @CacheKey('homepage_data')
+  @CacheTTL(0)
   @Get()
   async getHomepage(): Promise<any> {
     return this.homepageService.getHomepage();
